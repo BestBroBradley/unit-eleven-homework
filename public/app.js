40691518e0c3f9151ac39807aedacc5a15c30ba1
Allison Bradley <allison.bradley@outlook.com>
2020-01-24 12:03:41 -0700

    Fixed front and back end communication

--- public/app.js
+++ public/app.js
@@ -18,10 +18,6 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"))
 })
 
-//  * GET `*` - Should return the `index.html` file
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
-})
 
 //  * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
 
@@ -32,18 +28,24 @@ app.get("/api/notes", function (req, res) {
     res.json(dbJSON)
 })
 
+//  * GET `*` - Should return the `index.html` file
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
 //  * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post ("/api/notes", function (req, res) {
     let newNote = req.body;
-
-    fs.appendFile('../db/json.js', newNote, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!')
-        return res.json(dbJSON)
-      })
+    dbJSON.push(newNote);
+    console.log(newNote)
+    console.log(dbJSON)
+    res.json(newNote)
 })
 
 //  * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+app.delete ("/api/notes/:id", function (req, res) {
+
+})
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT)
